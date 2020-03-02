06b2c7f18ffe6f19d09258204c3b319fb9bd0aba
aftab70 <43289912+aftab70@users.noreply.github.com>
2020-03-02 13:18:25 +0530

    Update training.py

--- training.py
+++ training.py
@@ -17,5 +17,48 @@ list = ['A','F','T','A','B']
 
 print(list[1])
 
+#!/usr/env python3
+
+name = "Aftab"
+
+# A = 0   
+# f = 1
+# t = 2
+# a = 3
+# b = 4
+
+# b = -1
+# a = -2
+# t = -3
+# f = -4
+# A = -5
+
+print("")
+print("Start from beginning..")
+print("")
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+print("")
+print("start frm last digit")
+print("")
+
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+print(name[-5])
+
+print("")
+print("syntax - [start argument : stop argument]")
+print("")
+
+print(name[0:-2])
+
+
 ###############################################################################################################################
 
